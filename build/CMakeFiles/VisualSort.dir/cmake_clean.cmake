d9cf28afdff8e9f15f94a48773c4473fe737ead2
qiunian0207 <2719441481@qq.com>
2025-12-14 22:44:38 +0800

    first commit

---
+++ build/CMakeFiles/VisualSort.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VisualSort.dir/src/main.cpp.obj"
+  "CMakeFiles/VisualSort.dir/src/main.cpp.obj.d"
+  "VisualSort.exe"
+  "VisualSort.exe.manifest"
+  "VisualSort.pdb"
+  "libVisualSort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VisualSort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
